c3541e10075f8c64990cb711e56a1ff9709c6231
Tului Gantulga <tgantulga@ucdavis.edu>
2021-05-06 15:02:21 -0700

    working on login page, next is to actually log in

--- components/LoginPage.js
+++ components/LoginPage.js
@@ -4,26 +4,33 @@ import { View, Text, Image, StyleSheet, Button, TextInput } from 'react-native';
 class LoginPage extends React.Component {
   constructor(props) {
     super(props);
-    this.pressHandler = this.pressHandler.bind(this);
+    this.userLoginAction = this.userLoginAction.bind(this);
+    this.usernameInput = this.usernameInput.bind(this);
+    this.passwordInput = this.passwordInput.bind(this);
     this.state = {username: '', password: ''}
 
   }
 
   render() {
     return (
-      <View  style={styles.view}>
-        <Image source={require('../images/pantrypal.png')} style={styles.img}/>
-        <View>
-          <TextInput value={this.state.username} placeholder='Username' style={[styles.input, styles.username]}/>
-          <TextInput value={this.state.password} placeholder='Password' style={[styles.input, styles.password]}/>
+      <View style={styles.view}>
+        {/* <Image source={require('../images/pantrypal.png')} style={styles.img}/> */}
+        <View style={styles.view}>
+          <TextInput value={this.state.username} placeholder='Username' style={styles.input} onChangeText={this.usernameInput}/>
+          <TextInput value={this.state.password} placeholder='Password' style={styles.input} onChangeText={this.passwordInput} secureTextEntry={true}/>
         </View>
-        <Button title='Login' onPress={this.pressHandler}/>
+        <Button title='Login' onPress={this.userLoginAction}/>
+        <Button title='Demo' onPress={this.pressHandler}/>
       </View>
     )
   }
 
-  usernameInput() {
+  usernameInput(username) {
     this.setState({username})
+  }
+
+  passwordInput(password) {
+    this.setState({password})
   }
 
 
@@ -31,6 +38,12 @@ class LoginPage extends React.Component {
     const { navigation } = this.props;
     navigation.navigate('PantryPage')
   }
+
+  userLoginAction() {
+    console.log([this.state.username, this.state.password])
+  }
+
+
 
 }
 
@@ -45,10 +58,13 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 8,
     width: `50%`,
-    height: 40,
-    position: 'absolute',    
+    height: 40,  
   },
-
+  loginView: {
+    position: 'absolute',
+    marginTop: 40
+    // flex: 2
+  },  
 
   view: {
     paddingTop: 40,
