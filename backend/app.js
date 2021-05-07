2e5d86d396e23f3b914c04cb7d1b8d0e12b1e189
Tului <tgantulga@ucdavis.edu>
2021-05-07 11:55:08 -0700

    able to connect to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys_dev').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true})
+  .then(() => console.log('MongoDB Connected!!!!!!!!!!!!'))
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('SUp World!'));
 
