aac90d8a2d4ba52638fb90e29f5801468ac278a7
Tului <tgantulga@ucdavis.edu>
2021-05-07 15:23:42 -0700

    store is setup

--- App.js
+++ App.js
@@ -1,27 +1,46 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import Stacks from './src/components/Stacks';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import LoginPage from './src/components/LoginPage';
+import PantryPage from './src/components/PantryPage';
+import RecipesPage from './src/components/RecipesPage';
+import RootReducer from './src/reducers/root_reducer';
+
+const Stack = createStackNavigator();
+const store = createStore(RootReducer)
+
 
 class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.test}>
-        <Stacks/>
-      </View>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="LoginPage"
+              component={LoginPage}
+            />
+            <Stack.Screen
+              name="PantryPage"
+              component={PantryPage}
+            />
+            <Stack.Screen
+              name="RecipesPage"
+              component={RecipesPage}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+
     )
   }
 
 }
 
-
-const styles = StyleSheet.create({
-  test: {
-    width: `100%`,
-    height: `100%`,
-    backgroundColor: 'white'
-  }
-})
 
 
 
