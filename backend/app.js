d32e6a0f68b0caec323dc7b874436b74c861bb5c
Tului Gantulga <tgantulga@ucdavis.edu>
2021-05-10 20:49:31 -0700

    able to communicate with backend

--- backend/app.js
+++ backend/app.js
@@ -20,5 +20,6 @@ app.use('/api/recipes', recipes);
 
 
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
+const port  = 5000
 app.listen(port, () => console.log(`Listening on ${port}`));
