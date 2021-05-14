80f5a6be40337544b7c487efc8848ea7b2bdc54c
Tului Gantulga <tgantulga@ucdavis.edu>
2021-05-13 22:06:37 -0700

    need to work on textinput  events

--- src/components/PantryPage.js
+++ src/components/PantryPage.js
@@ -9,7 +9,10 @@ class PantryPage extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {searching: ''}
+
     // all bindings
+    this.updateSearch = this.updateSearch.bind(this);
     this.userLogoutAction = this.userLogoutAction.bind(this);
 
   }
@@ -24,24 +27,23 @@ class PantryPage extends React.Component {
     )
   }
 
-
-  searchComponent() {
-    return (
-      <View>
-        <TextInput style={styles.input} placeholder='Search for ingredients...' />
-      </View>
-    )
-  }
-
-
-
   userLogoutAction() {
     const {navigation, logout} = this.props;
     logout();
     navigation.navigate('LoginPage')
   }
 
-  
+  searchComponent() {
+    return (
+      <View>
+        <TextInput style={styles.input} placeholder='Search for ingredients...' value={this.state.searching} onChangeText={this.updateSearch}/>
+      </View>
+    )
+  }
+
+  updateSearch(newSearchVal) {
+    this.setState({searching: newSearchVal})
+  }
 
 
 }
