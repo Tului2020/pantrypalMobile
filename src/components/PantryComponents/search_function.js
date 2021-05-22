080adade0754ee3678fd05970c28eb0b31be8918
Tului <tgantulga@ucdavis.edu>
2021-05-21 22:36:05 -0700

    search function working

--- src/components/PantryComponents/search_function.js
+++ src/components/PantryComponents/search_function.js
@@ -1,5 +1,24 @@
 // import allIngredients from './search_data';
-const allIngredients = require('./search_data')
+const ingredients = require('./search_data')
+
+const searchIngredient = (searchQuery) => {
+  let names = {}
+
+  ingredients.forEach(ingredient => {
+    names[ingredient.name] = ingredient;
+  })
 
 
-console.log(allIngredients)
+  let queryArrayPrimary = [];
+  let queryArraySecondary = [];
+
+  Object.keys(names).forEach(ingredientName => {
+    if (ingredientName.search(searchQuery) === 0) queryArrayPrimary.push(names[ingredientName])
+    if (ingredientName.search(searchQuery) > 0) queryArraySecondary.push(names[ingredientName])
+  })
+
+  return queryArrayPrimary.concat(queryArraySecondary);
+}
+
+
+console.log(searchIngredient('apple'))
